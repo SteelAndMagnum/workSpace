39f7e44640dcbdd4370962f4c2a29cdf627c2deb
steelandmagnum <steelandmagnum@steelandmagnums-MacBook-Pro.local>
2020-06-26 19:47:30 -0600

    FIRST COMMITTED PROJECT

---
+++ WorkSpace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkSpace
+//
+//  Created by steelandmagnum on 6/26/20.
+//  Copyright © 2020 steelandmagnum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
